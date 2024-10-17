8f8652e0586b944b2a14f29f89889cb48d6a9173
Rrkhaa <168824333+Rrkhaa@users.noreply.github.com>
2024-10-17 14:59:18 +0700

    Add files via upload

---
+++ Nested Classes/Mouter.java
@@ -0,0 +1,26 @@
+public class Mouter {
+    private int m = (int) (Math.random()*100);
+ 
+    public void go(int x, final int y) {
+        int a = x + y;
+        final int b = x - y;
+
+        class MInner {
+            public void method() {
+                System.out.println("m = " + m);
+                System.out.println("x = " + x);
+                System.out.println("y = " + y);
+                System.out.println("a = " + a);
+                System.out.println("b = " + b);
+            }
+        }
+
+        MInner that = new MInner();
+        that.method();
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Mouter that = new Mouter();
+        that.go((int)(Math.random()*100), (int)(Math.random()*100));
+    }
+}
